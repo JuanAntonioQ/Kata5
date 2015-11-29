4456e832dd5de6ec8aa1cc42c534464f1808edc6
Juan Antonio <Juan Antonio@JuanAntonio-PC>
2015-11-29 21:41:32 +0000

    Third Version

--- src/kata5/Kata5.java
+++ src/kata5/Kata5.java
@@ -1,5 +1,10 @@
 package kata5;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -7,11 +12,22 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Kata5 {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException{
-            Class.forName("oracle.jdbc.driver.OracleDriver");
-            Connection c = DriverManager.getConnection("jdbc:oracle:thin:@10.22.143.90:1521:orcl", "system", "orcl");
+    public static void main(String[] args) throws SQLException, ClassNotFoundException, FileNotFoundException, IOException{
+            Class.forName("org.sqlite.JDBC");
+            Connection c = DriverManager.getConnection("jdbc:sqlite:KATADB");
+            
             Statement stmt = c.createStatement();
+            String filename = "C:\\Users\\Juan Antonio\\Documents\\NetBeansProjects\\Kata5\\DATA\\mail.txt";
+            BufferedReader reader = new BufferedReader(new FileReader(new File(filename)));
+            
+            String mail;
+            while ((mail = reader.readLine()) != null){
+                String query = "INSERT INTO MAILS (MAIL) VALUES ('" + mail + "')";
+                stmt.executeUpdate(query);
+            }
+            
             ResultSet rs = stmt.executeQuery("SELECT * FROM PEOPLE");
+            
             while (rs.next()){
                 System.out.println("ID = " + rs.getInt("ID"));
                 System.out.println("NAME = " + rs.getString("NAME"));
@@ -20,6 +36,5 @@ public class Kata5 {
             rs.close();
             stmt.close();
             c.close();
-        
     }
 }
