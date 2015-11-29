3b571423fd46b6bb12150c7b3b00be26ac828e48
Juan Antonio <Juan Antonio@JuanAntonio-PC>
2015-11-29 20:53:59 +0000

    Second Version

--- src/kata5/Kata5.java
+++ src/kata5/Kata5.java
@@ -8,8 +8,8 @@ import java.sql.Statement;
 
 public class Kata5 {
     public static void main(String[] args) throws SQLException, ClassNotFoundException{
-            Class.forName("org.sqlite.JDBC");
-            Connection c = DriverManager.getConnection("jdbc:sqlite:KATADB");
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            Connection c = DriverManager.getConnection("jdbc:oracle:thin:@10.22.143.90:1521:orcl", "system", "orcl");
             Statement stmt = c.createStatement();
             ResultSet rs = stmt.executeQuery("SELECT * FROM PEOPLE");
             while (rs.next()){
